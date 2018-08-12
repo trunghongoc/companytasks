e1bc7988a1f35628c21f7d33bb06ce3de6768192
Ho Ngoc Trung <trung0194@gmail.com>
2018-08-12 13:24:40 +0700

    Add style is-dragging is-dragging-over

--- src/components/TaskColumn.js
+++ src/components/TaskColumn.js
@@ -3,17 +3,25 @@ import { Draggable } from 'react-beautiful-dnd'
 // import NullComponent from './../containers/NullComponent'
 
 class TaskColumn extends Component {
+    isDraggingClass = (yes) => {
+        if (yes) {
+            return "task-column task-is-dragging"
+        } else {
+            return "task-column"
+        }
+    }
+
     render() {
         let { task, index } = this.props
         return(
             <Draggable key={task.id} draggableId={task.id} index={index}>
-                {(provided) => (
+                {(provided, snapshot) => (
                     <div
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                     >
-                        <div className="task-column">
+                        <div className={this.isDraggingClass(snapshot.isDragging)} >
                             {task.content}
                         </div>
                     </div>
--- src/containers/CardColumn.js
+++ src/containers/CardColumn.js
@@ -3,25 +3,32 @@ import { Droppable } from 'react-beautiful-dnd'
 import TaskColumn from './../components/TaskColumn'
 
 class CardColumn extends Component {
+    isDraggingClass = (yes) => {
+        if (yes) {
+            return "card-column-task-list task-is-draggingOver-on-board"
+        } else {
+            return "card-column-task-list"
+        }
+    }
+
     render() {
         let { column, tasks } = this.props
         return(
             <div className="card-column">
                 <div className="card-column-title">{column.title}</div>
-                <div className="card-column-task-list">
-                    <Droppable droppableId={column.id}>
-                        {(provided) => (
-                            <div
-                                ref={provided.innerRef}
-                            >
-                                {tasks.map((task, index) => (
-                                    <TaskColumn key={task.id} task={task} index={index} />
-                                ))}
-                                {provided.placeholder}
-                            </div>
-                        )}
-                    </Droppable>
-                </div>
+                <Droppable droppableId={column.id}>
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            className={this.isDraggingClass(snapshot.isDraggingOver)}
+                        >
+                            {tasks.map((task, index) => (
+                                <TaskColumn key={task.id} task={task} index={index} />
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
             </div>
         )
     }
